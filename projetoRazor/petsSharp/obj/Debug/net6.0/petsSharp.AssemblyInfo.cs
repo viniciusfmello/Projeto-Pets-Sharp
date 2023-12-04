f9129190c80a0bf903e4be71c068a5895235fe75
Yago Henrique <yagogamer82@gmail.com>
2023-12-03 22:16:06 -0300

    alteração maneira

--- projetoRazor/petsSharp/obj/Debug/net6.0/petsSharp.AssemblyInfo.cs
+++ projetoRazor/petsSharp/obj/Debug/net6.0/petsSharp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("petsSharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69fcca27ef2981728801c5d48bdae395d55cf0b8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("petsSharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("petsSharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
