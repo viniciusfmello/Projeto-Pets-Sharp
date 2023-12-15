dee7c8e9fe95b23c710cb5349c2c7c39adba7fbd
Yago Henrique <yagogamer82@gmail.com>
2023-12-14 23:16:56 -0300

    .

---
+++ projetoRazor/petsSharp/Pages/indexVendas.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace petsSharp.Pages
+{
+    public class indexVendasModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
